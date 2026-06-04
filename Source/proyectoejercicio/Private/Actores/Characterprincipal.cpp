598d0f475c8f7a49d7e688d8830cfa0ce487f896
Juan Manuel Cabral <juanmacabral9@gmail.com>
2026-06-04 02:59:54 -0300

    cambios en replicacion
    arreglando bugs del proyecto y la replicacion rota

--- Source/proyectoejercicio/Private/Actores/Characterprincipal.cpp
+++ Source/proyectoejercicio/Private/Actores/Characterprincipal.cpp
@@ -1,98 +1,158 @@
-﻿#include "Actores/Characterprincipal.h"
+#include "Actores/Characterprincipal.h"
 #include "EnhancedInputComponent.h"
-#include "EnhancedInputSubsystems.h" 
+#include "EnhancedInputSubsystems.h"
 #include "InputActionValue.h"
 #include "GameFramework/PlayerController.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "Actores/WeaponMaster.h"
 
-// Constructor
+
 ACharacterprincipal::ACharacterprincipal()
 {
-    PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
 
-    // 1. Configuración de rotación
-    bUseControllerRotationPitch = false;
-    bUseControllerRotationYaw = false;
-    bUseControllerRotationRoll = false;
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
 
-    // 2. Configuración del Movimiento
-    GetCharacterMovement()->bOrientRotationToMovement = true;
-    GetCharacterMovement()->RotationRate = FRotator(0.0f, 400.0f, 0.0f);
-    GetCharacterMovement()->bUseControllerDesiredRotation = false;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 400.0f, 0.0f);
+	GetCharacterMovement()->bUseControllerDesiredRotation = false;
+	GetCharacterMovement()->MaxWalkSpeed = 400.0f;
+	GetCharacterMovement()->JumpZVelocity = 250.0f;
+	GetCharacterMovement()->GravityScale = 2.2f;
+	JumpMaxCount = 1;
 }
 
-// BeginPlay
 void ACharacterprincipal::BeginPlay()
 {
-    Super::BeginPlay();
+	Super::BeginPlay();
 }
 
-// Tick (Esta es la que te daba error LNK2001 por faltar)
 void ACharacterprincipal::Tick(float DeltaTime)
 {
-    Super::Tick(DeltaTime);
+	Super::Tick(DeltaTime);
+}
+void ACharacterprincipal::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ACharacterprincipal, CurrentWeapon);
+}
+void ACharacterprincipal::OnRep_CurrentWeapon()
+{
 }
 
-// SetupPlayerInputComponent
+void ACharacterprincipal::Server_EquipWeapon_Implementation(AWeaponMaster* Weapon)
+{
+	if (!Weapon) return;
+
+	DropCurrentWeapon(); 
+	CurrentWeapon = Weapon;
+	CurrentWeapon->DisablePickup();
+	CurrentWeapon->AttachToComponent(GetMesh(),FAttachmentTransformRules::SnapToTargetNotIncludingScale,Weapon->SocketName
+	);
+}
+
+void ACharacterprincipal::Server_DropWeapon_Implementation()
+{
+	DropCurrentWeapon();
+}
+
 void ACharacterprincipal::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-    Super::SetupPlayerInputComponent(PlayerInputComponent);
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-    if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
-    {
-        if (IA_Movimiento)
-        {
-            EnhancedInput->BindAction(IA_Movimiento, ETriggerEvent::Triggered, this, &ACharacterprincipal::Move);
-        }
-    }
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (IA_Movimiento)
+		{
+			EnhancedInput->BindAction(IA_Movimiento, ETriggerEvent::Triggered, this, &ACharacterprincipal::Move);
+		}
+		if (IA_Interactuar)
+			EnhancedInput->BindAction(IA_Interactuar, ETriggerEvent::Triggered,	this, &ACharacterprincipal::TryInteract);
+	}
 }
 
-// Lógica de Movimiento Corregida
 void ACharacterprincipal::Move(const FInputActionValue& Value)
 {
-    const FVector2D InputVec = Value.Get<FVector2D>();
+	const FVector2D InputVec = Value.Get<FVector2D>();
 
-    if (!Controller) return;
+	if (!Controller) return;
 
-    // --- EJE X (A y D): Rota la CÁMARA ---
-    if (InputVec.X != 0.0f)
-    {
-        AddControllerYawInput(InputVec.X);
-    }
+	if (InputVec.X != 0.0f)
+	{
+		AddControllerYawInput(InputVec.X);
+	}
 
-    // --- EJE Y (W y S): Mueve al PERSONAJE ---
-    if (InputVec.Y != 0.0f)
-    {
-        const FRotator Rotation = Controller->GetControlRotation();
-        const FRotator YawRotation(0, Rotation.Yaw, 0);
-        const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	if (InputVec.Y != 0.0f)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
 
-        AddMovementInput(ForwardDirection, InputVec.Y);
-    }
+		AddMovementInput(ForwardDirection, InputVec.Y);
+	}
 }
 
-// Interfaz Monedas (Esta faltaba y daba error LNK2001)
 void ACharacterprincipal::AddMonedas_Implementation(int Moneda)
 {
-    Monedas += Moneda;
+	Monedas += Moneda;
 
-    if (GEngine)
-    {
-        GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, FString::Printf(TEXT("Monedas: %d"), Monedas));
-    }
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, FString::Printf(TEXT("Monedas: %d"), Monedas));
+	}
 }
 
 USkeletalMeshComponent* ACharacterprincipal::GetSkeletalMesh_Implementation()
 {
-    return GetMesh();
-
+	return GetMesh();
 }
 
-// Función Mostrar Mensaje (Esta faltaba y daba error LNK2019)
 void ACharacterprincipal::MostrarMensaje()
 {
-    if (GEngine)
-    {
-        GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, TEXT("Overlapeando"));
-    }
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, TEXT("Overlapeando"));
+	}
+}
+
+void ACharacterprincipal::TryInteract()
+{
+	if (CurrentWeapon)
+	{
+		TArray<AActor*> Overlapping;
+		GetOverlappingActors(Overlapping, AWeaponMaster::StaticClass());
+
+		bool bNearPickup = false;
+		for (AActor* Actor : Overlapping)
+		{
+			AWeaponMaster* Weapon = Cast<AWeaponMaster>(Actor);
+			if (Weapon && Weapon != CurrentWeapon)
+			{
+				bNearPickup = true;
+				break;
+			}
+		}
+
+		if (!bNearPickup)
+			Server_DropWeapon();
+	}
+}
+
+void ACharacterprincipal::DropCurrentWeapon()
+{
+	if (!CurrentWeapon) return;
+
+	FVector DropLocation = GetActorLocation() + GetActorForwardVector() * 150.f;  // más lejos
+
+	CurrentWeapon->DetachFromActor(
+		FDetachmentTransformRules::KeepWorldTransform
+	);
+	CurrentWeapon->SetActorLocationAndRotation(DropLocation, FRotator::ZeroRotator);
+
+	CurrentWeapon->EnablePickupDelayed(1.0f); 
+	CurrentWeapon = nullptr;
 }
---
+++ Source/proyectoejercicio/Private/Actores/WeaponMaster.cpp
@@ -0,0 +1,61 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+#include "Actores/WeaponMaster.h"
+#include "Actores/Characterprincipal.h"
+#include "Components/SphereComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+AWeaponMaster::AWeaponMaster()
+{
+	bReplicates = true;
+	SetReplicatingMovement(true);
+
+	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>("WeaponMesh");
+	SetRootComponent(WeaponMesh);
+	WeaponMesh->SetSimulatePhysics(false);
+	WeaponMesh->SetEnableGravity(false);
+	WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	PickupCollision = CreateDefaultSubobject<USphereComponent>("PickupCollision");
+	PickupCollision->SetupAttachment(WeaponMesh);
+	PickupCollision->SetSphereRadius(80.f);
+	PickupCollision->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+}
+
+void AWeaponMaster::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AWeaponMaster::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	if (!HasAuthority() || !OtherActor) return;
+
+	ACharacterprincipal* Character = Cast<ACharacterprincipal>(OtherActor);
+	if (!Character) return;
+
+
+	PickupCollision->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	Character->Server_EquipWeapon(this);
+}
+
+void AWeaponMaster::EnablePickup()
+{
+	PickupCollision->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	FVector Loc = GetActorLocation();
+	SetReplicatingMovement(true);
+	SetActorLocation(FVector(Loc.X, Loc.Y, Loc.Z - 50.f));
+}
+
+void AWeaponMaster::DisablePickup()
+{
+	PickupCollision->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	SetReplicatingMovement(false);
+}
+
+void AWeaponMaster::EnablePickupDelayed(float Delay)
+{
+	GetWorldTimerManager().SetTimer(PickupTimerHandle,this,&AWeaponMaster::EnablePickup,Delay,false);
+}
